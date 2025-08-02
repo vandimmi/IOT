cca33dc77b49f752a00a0da5a08d1b7990ee48b4
vandimmi <vtdmy23@clc.fitus.edu.vn>
2025-08-02 17:12:46 +0700

    add alarm button

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { bot } from './telegram/telegram.bot';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);

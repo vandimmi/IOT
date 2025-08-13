2cd2d8763db80fa37a324764f8bbe451d3f0456e
NPT-102 <nptho23@clc.fitus.edu.vn>
2025-08-13 11:15:43 +0700

    final bot

--- back-end/src/helper/chatbot.utils.ts
+++ back-end/src/helper/chatbot.utils.ts
@@ -1,13 +1,13 @@
 const blind_replies = [
-	"Please try something more descriptive.",
-	"Oh! It appears you wrote something I don't understand yet.",
-	"Do you mind trying to rephrase that?",
-	"I'm terribly sorry, I didn't quite catch that.",
-	"I can't answer that yet, please try asking something else."
+  "Please try something more descriptive.",
+  "Oh! It appears you wrote something I don't understand yet.",
+  "Do you mind trying to rephrase that?",
+  "I'm terribly sorry, I didn't quite catch that.",
+  "I can't answer that yet, please try asking something else."
 ];
 
 const response_patterns = [
-	{
+  {
     "response_type": "greeting",
     "user_input": ["hello", "hi", "hey"],
     "bot_response": "<p>Hey there!</p>",
@@ -42,56 +42,59 @@ const response_patterns = [
     "required_words": ["/help"]
   },
   {
-    "response_type": "hyperlink",
-    "user_input": ["show", "setting"],
-    "bot_response": "<p>Come with me! <a href='http://localhost:5500/front-end/pages/setting.html'>Here</a>.</p>",
-    "required_words": ["setting"]
-  },
-  {
-    "response_type": "hyperlink",
-    "user_input": ["profile"],
-    "bot_response": `<p>Come with me! <a href='http://localhost:5500/front-end/pages/profile.html'>Here</a>.</p>`,
-    "required_words": ["profile"]
-  },
+    "response_type": "helpall",
+    "user_input": ["/helpall"],
+    "bot_response": `
+    <p>Hướng dẫn đăng ký và xác nhận tài khoản để kích hoạt thiết bị:</p>
+    <ol>
+      <li>Đăng ký tài khoản và xác nhận email để kích hoạt.</li>
+      <li>Đăng nhập vào trang dashboard để quan sát tình hình máy đo.</li>
+      <li>Để kết nối thiết bị, vui lòng kết nối Wi-Fi tên <b>ESP32_Config</b> với mật khẩu <b>12345678</b>.</li>
+      <li>Nhập mạng Wi-Fi muốn thiết bị kết nối trên trang web.</li>
+      <li>Vào trang Profile và bấm Save để lưu thông tin kết nối, hệ thống sẽ xác nhận kết nối thiết bị.</li>
+    </ol>
+  `,
+    "required_words": ["/helpall"]
+  }
 ]
 
 function blindResponse() {
-	let rand_idx = Math.floor(Math.random() * blind_replies.length);
-	return blind_replies[rand_idx];
+  let rand_idx = Math.floor(Math.random() * blind_replies.length);
+  return blind_replies[rand_idx];
 }
 
 export function generateResponse(input: string) {
-	const words = input.toLocaleLowerCase('vi').split(/\s+|[,;?!.-]\s*/);
+  const words = input.toLocaleLowerCase('vi').split(/\s+|[,;?!.-]\s*/);
 
-	let response_rank: number[] = [];
+  let response_rank: number[] = [];
 
-	for (let pattern of response_patterns) {
-		let score = 0;
-		let requirement_check = true;
+  for (let pattern of response_patterns) {
+    let score = 0;
+    let requirement_check = true;
 
-		for (let required_word of pattern["required_words"]) {
-			requirement_check &&= words.includes(required_word);
-		}
+    for (let required_word of pattern["required_words"]) {
+      requirement_check &&= words.includes(required_word);
+    }
 
-		if (requirement_check) {
-			for (let word of words) {
-				if (pattern["user_input"].includes(word))
-					score++;
-			}
-		}
+    if (requirement_check) {
+      for (let word of words) {
+        if (pattern["user_input"].includes(word))
+          score++;
+      }
+    }
 
-		response_rank.push(score);
-	}
+    response_rank.push(score);
+  }
 
-	let best_response_idx = 0;
-	for (let i = 0; i < response_rank.length; i++) {
-		if (response_rank[i] > response_rank[best_response_idx])
-			best_response_idx = i;
-	}
+  let best_response_idx = 0;
+  for (let i = 0; i < response_rank.length; i++) {
+    if (response_rank[i] > response_rank[best_response_idx])
+      best_response_idx = i;
+  }
 
-	if (response_rank[best_response_idx] > 0) {
-		return response_patterns[best_response_idx]["bot_response"];
-	}
-	
-	return blindResponse();
+  if (response_rank[best_response_idx] > 0) {
+    return response_patterns[best_response_idx]["bot_response"];
+  }
+
+  return blindResponse();
 }
